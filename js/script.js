f8a1ecebca4f8edd10ea3e1119ebcb629c57ef0a
Your Name <naorhl123@gmail.com>
2023-06-17 22:12:00 +0300

    make it better

---
+++ js/script.js
@@ -0,0 +1,34 @@
+ScrollReveal().reveal('.headline');
+
+var slideUp = {
+  origin: 'bottom',
+  scale: 1.1,
+  easing: 'ease-in',
+  duration: 500,
+  delay: 250
+};
+
+
+ScrollReveal().reveal('.slide-up', slideUp);
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+const scrubTextElements = document.querySelectorAll(".scrubText");
+
+scrubTextElements.forEach((element) => {
+  gsap.to(element, {
+    y: 30,
+    scale: 0.85,
+    opacity: 1,
+    scrollTrigger: {
+      trigger: element,
+      start: "top 75%",
+      end: `+=${element.offsetHeight + 80}`,
+      scrub: 0.5,
+      toggleActions: "restart none none none",
+      markers: true,
+      toggleClass: "red",
+    }
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px'
+      },
+      animation: {
+        fadeIn: "fadeIn 1.5s ease-in forwards",
+        fadeOut: "fadeOut 1.5s 1s ease-out forwards",
+        variants: {
+          animation: ["motion-safe"]
+      }
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        },
+        fadeOut: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 }
+        },
+      },
+      colors:{
+        black: '#080A09',
+        lightBlack: '#1E1E1E',
+        cherry: '#D2042D',
+      },
+      fontFamily: {
+        Assistant: "Assistant",
+      },
+    },
+  },
+  plugins: [],
+}
+
