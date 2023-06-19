1603bf3b3f613510f865c8807206b8bac7efba72
naorHL1 <naorhl123@gmail.com>
2023-06-20 02:22:24 +0300

    font fix

--- css/js/script.js
+++ css/js/script.js
@@ -35,10 +35,8 @@ scrubTextElements.forEach((element) => {
 
 $(document).ready(function(){
 
-	// Init ScrollMagic
 	var controller = new ScrollMagic.Controller();
 
-	// Scene 1 - pin the second section
 	var pinScene01 = new ScrollMagic.Scene({
 		triggerElement: '#slide01',
 		triggerHook: 0,
