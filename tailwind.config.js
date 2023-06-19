778d2abc7a099007852e49e415bb3e2fd38f2531
naorHL1 <naorhl123@gmail.com>
2023-06-19 23:43:15 +0300

    another commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         lg: '976px',
         xl: '1440px'
       },
+      backgroundImage: {
+        'gradientGrey': 'linear-gradient(180deg, #1A1B1D 0%, #1A1B1D 100%)',
+        'sea2': 'url("http://porschevolution.com/images/slides/2000.jpg")',
+        'sea3': 'url("http://porschevolution.com/images/slides/2010.jpg")'
+      },
       animation: {
         fadeIn: "fadeIn 1.5s ease-in forwards",
         fadeOut: "fadeOut 1.5s 1s ease-out forwards",
