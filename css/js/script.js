fb601b0b1dffad16be8b6f18682460396d6a986e
naorHL1 <naorhl123@gmail.com>
2023-06-24 14:14:29 +0300

    nav menu

--- css/js/script.js
+++ css/js/script.js
@@ -13,12 +13,10 @@ const mobileNav = () => {
 		mobileNav.style.display = 'flex';
 		document.body.style.overflowY = 'hidden';
 		document.body.style.position = 'fixed';
-		document.body.style.backgroundColor = '#ffffff';
 	  } else {
 		mobileNav.style.display = 'none';
 		document.body.style.overflowY = 'auto';
 		document.body.style.position = 'static';
-		document.body.style.backgroundColor = 'initial';
 	  }
 	});
   
